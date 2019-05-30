eca26d7a3a8de52564183bd282e854e343787565
shimizu takahiro <ShimizuTakahiro>
2019-05-30 17:43:08 +0900

    helloWorldに条件分岐を追記

--- helloWorld.c
+++ helloWorld.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 
-void print_hello(){
+void print_hello1(){
 
-  printf("Hello, world!\n");
+  printf("Hello, world1!\n");
+
+}
+
+void print_hello2(){
+
+  printf("Hello, world2!\n");
 
 }
 
 int main(int argc, char *args[])
 {
-  print_hello();
+  int x;
+
+  printf("input x's value\n");
+  scanf("%d", &x);
+
+  if(x < 10){
+    print_hello1();
+  }
+  else{
+    print_hello2();
+  }
 
   return 0;
 }
