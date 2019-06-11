645ad88623246df81de8423819d70ef8e72b7f11
shimizu takahiro <ShimizuTakahiro>
2019-06-12 01:53:50 +0900

    ダミー関数をさらに入れてみた

--- source_101.c
+++ source_101.c
@@ -9,6 +9,13 @@ int b;
 void dummy1();
 void dummy2();
 void dummy3();
+void dummy4();
+void dummy5();
+void dummy6();
+void dummy7();
+void dummy8();
+void dummy9();
+void dummy10();
   
 void main(void)
 {
@@ -43,29 +50,36 @@ void B(void)
       case(1):
         if(d == 1){
           a = 2;
+	  dummy4();
         }
         else if (d == 2){
           a = 0;
+	  dummy5();
         }
         else{
           printf("message2\n");
+	  dummy6();
         }
         break;
 
       case(2):
         if(d == 1){
           a = 0;
+	  dummy7();
         }
         else if (d == 2){
           a = 1;
+	  dummy8();
         }
         else{
           printf("messsage2\n");
+	  dummy9();
         }
         break;
 
         default:
           printf("message3\n");
+	  dummy10();
     }
   }
 
@@ -86,4 +100,25 @@ void dummy2(){
 }
 
 void dummy3(){
+}
+
+void dummy4(){
+}
+
+void dummy5(){
+}
+
+void dummy6(){
+}
+
+void dummy7(){
+}
+
+void dummy8(){
+}
+
+void dummy9(){
+}
+
+void dummy10(){
 }
