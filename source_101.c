559fe9442e75db9196d3416b6084c5594ff21580
shimizu takahiro <ShimizuTakahiro>
2019-09-26 15:56:28 +0900

    2回目の状態遷移のdummy関数をなくした

--- source_101.c
+++ source_101.c
@@ -16,16 +16,6 @@ void dummy7();
 void dummy8();
 void dummy9();
 void dummy10();
-void dummy11();
-void dummy12();
-void dummy13();
-void dummy14();
-void dummy15();
-void dummy16();
-void dummy17();
-void dummy18();
-void dummy19();
-void dummy20();
   
 void main(void)
 {
@@ -103,51 +93,41 @@ void B(void)
       case(0):
         if(d == 1){
           a = 1;
-	  dummy11();
         }
         else if (d == 2){
           a = 2;
-	  dummy12();
         }
         else{
           printf("message2\n");
-	  dummy13();
         }
         break;
   	
       case(1):
         if(d == 1){
           a = 2;
-	  dummy14();
         }
         else if (d == 2){
           a = 0;
-	  dummy15();
         }
         else{
           printf("message2\n");
-	  dummy16();
         }
         break;
 
       case(2):
         if(d == 1){
           a = 0;
-	  dummy17();
         }
         else if (d == 2){
           a = 1;
-	  dummy18();
         }
         else{
           printf("messsage2\n");
-	  dummy19();
         }
         break;
 
         default:
           printf("message3\n");
-	  dummy20();
     }
   }
 
